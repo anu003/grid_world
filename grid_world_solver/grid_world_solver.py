dbf616812a32fc95bd5674dda4cb7463159cc341
Kyle Hollins Wray <kyle.hollins.wray@gmail.com>
2014-12-06 13:09:35 -0500

    Quick python implementation that solves the grid worlds output by the generator application.

--- grid_world_solver/grid_world_solver.py
+++ grid_world_solver/grid_world_solver.py
@@ -20,5 +20,83 @@
     CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
+import sys
 import csv
+import numpy as np
+
+# Load the file from standard input.
+class GridWorldMDP(object):
+    """ The GridWorldMDP class which loads a raw grid world file, and provides value iteration to solve it. """
+    
+    def __init__(self, filename):
+        """ The constructor for the GridWorldMDP class, which loads a grid world MDP from a file.
+        
+            Parameters:
+                filename    The grid world filename.
+        """
+        
+        # Load all the data in this object.
+        data = list()
+        with open(filename, 'r') as f:
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                data += [list(row)]
+        
+        # Attempt to parse all the data into their respective variables.
+        try:
+            self.n = int(data[0][0])
+            self.m = 4
+            self.R = [float(data[i + 1][0]) for i in range(self.n)]
+            self.T = [[[float(data[(self.n + 1) + self.n * a + s][sp]) for sp in range(self.n)] for a in range(self.m)] for s in range(self.n)]
+        except Exception:
+            print("Failed to load file.")
+            raise Exception()
+
+
+    def solve(self, gamma=0.9, epsilon=0.01):
+        """ Solve the GridWorldMDP using value iteration.
+        
+            Parameters:
+                gamma      The discount factor on [0, 1].
+                epsilon    The tolerance for convergence.
+                
+            Returns:
+                V        The value of the states.
+                pi       The policy of each of the states.
+        """
+        
+        delta = 1.0 + epsilon
+        
+        self.V = [0.0 for i in range(self.n)]
+        self.pi = [0 for i in range(self.n)]
+        
+        convergenceCriterion = epsilon
+        if gamma < 1.0:
+            convergenceCriterion *= (1.0 - gamma) / gamma
+        
+        while delta > convergenceCriterion:
+            delta = 0.0
+            for s in range(self.n):
+                Qsa = [np.sum([self.T[s][a][sp] * self.V[sp] for sp in range(self.n)]) for a in range(self.m)]
+                Vs = self.R[s] + gamma * np.max(Qsa)
+                delta = max(delta, abs(Vs - self.V[s]))
+                self.V[s] = Vs
+                self.pi[s] = np.argmax(Qsa)
+        
+        return self.V, self.pi
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please specify a file to load.")
+    else:
+        mdp  = GridWorldMDP(sys.argv[1])
+        mdp.solve(1.0, 0.0001)
+        print(mdp.V)
+        print(mdp.pi)
+
+
+
+
+
 
